59afad81184c06963ab4b18ffd6ed073ac22a5b1
Ganesha Ram G <125204007+ganesha62@users.noreply.github.com>
2025-09-03 22:22:25 +0530

    Update app.module.ts for prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { Job } from './jobs/job.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5433'),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'jobmanagement',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+       ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
       entities: [Job],
       synchronize: true,
       logging: true,
