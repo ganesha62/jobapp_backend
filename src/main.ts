7912842082105b73af4074721e5cd46b6f67471c
Ganesha Ram G <125204007+ganesha62@users.noreply.github.com>
2025-09-04 09:48:25 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 app.enableCors({
   origin: [
     'http://localhost:3000',         // local dev
-    'https://jobapp-phi.vercel.app/',
+    'https://jobapp-phi.vercel.app',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
